273aa5cbeb029da0a4c2a03430fc396ad9991188
garyk-colo <54647559+garyk-colo@users.noreply.github.com>
2019-10-16 15:13:13 -0700

    Update providers.tf

--- modules/eks/providers.tf
+++ modules/eks/providers.tf
@@ -5,8 +5,8 @@ variable "aws-region" {}
 
 provider "aws" {
   region = var.aws-region
-  access_key = ${var.aws_access_key}
-  secret_key = ${var.aws_secret_key}
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
 provider "http" {}
