6bc7785743adf246381a752031f368222bf4e63c
garyk-colo <54647559+garyk-colo@users.noreply.github.com>
2019-10-17 13:58:43 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,5 @@
 # Variables Configuration
 
-variable "access_key" {
-  default     = "${var.aws_access_key}"
-  type        = string
-  description = "AWS Access Key"
-}
-
-variable "secret_key" {
-  default     = "${var.aws_secret_key}"
-  type        = string
-  description = "AWS Secert Key"
-}
-
 variable "cluster-name" {
   default     = "colosseumusa-tf"
   type        = string
