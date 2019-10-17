44e8dacd358e6d39723998ee8da72b0c3944ed56
garyk-colo <54647559+garyk-colo@users.noreply.github.com>
2019-10-17 15:44:46 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,13 +24,3 @@ module "eks" {
   eks-cw-logging           = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   ec2-key-public-key       = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
 }
-
-output "kubeconfig" {
-  value = module.eks.kubeconfig
-}
-
-output "config-map" {
-  value = module.eks.config-map
-}
-}
-
