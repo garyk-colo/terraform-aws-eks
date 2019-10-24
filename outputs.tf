da8ebefec0164398a9dae963e60586daa894c6e1
garyk-colo <54647559+garyk-colo@users.noreply.github.com>
2019-10-24 15:34:13 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,6 +9,6 @@ output "config-map" {
 }
 
 output "jx_params" {
-  value = "--provider=eks --gitops --no-tiller --vault --aws-dynamodb-region=${var.aws-region} --aws-dynamodb-table=${aws_dynamodb_table.vault-data.name} --aws-kms-=${var.region} --aws-kms-key-id=${aws_kms_key.bank_vault.key_id} --aws-s3-region=${var.aws-region}  --aws-s3-bucket=${aws_s3_bucket.vault-unseal.id} --aws-access-key-id=${aws_iam_access_key.vault.id} --aws-secret-access-key=${aws_iam_access_key.vault.secret}"
+  value = "--provider=eks --gitops --no-tiller --vault --aws-dynamodb-region=${var.aws-region} --aws-dynamodb-table=${aws_dynamodb_table.vault-data.name} --aws-kms-=${var.aws-region} --aws-kms-key-id=${aws_kms_key.bank_vault.key_id} --aws-s3-region=${var.aws-region}  --aws-s3-bucket=${aws_s3_bucket.vault-unseal.id} --aws-access-key-id=${aws_iam_access_key.vault.id} --aws-secret-access-key=${aws_iam_access_key.vault.secret}"
   description = "Output KMS key id, S3 bucket name and secret name in the form of jx install options"
 }
