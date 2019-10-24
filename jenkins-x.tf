fa45e559a1490ad3f9eb6d8375efa6287fcdda05
garyk-colo <54647559+garyk-colo@users.noreply.github.com>
2019-10-24 15:27:07 -0700

    Create jenkins-x.tf

---
+++ jenkins-x.tf
@@ -0,0 +1,102 @@
+# Create S3 bucket for KMS
+resource "aws_s3_bucket" "vault-unseal" {
+    bucket = "vault-unseal.${var.region}.${var.bucket_domain}"
+    acl    = "private"
+
+    versioning {
+        enabled = false
+    }
+}
+
+# Create KMS key
+resource "aws_kms_key" "bank_vault" {
+    description = "KMS Key for bank vault unseal"
+}
+
+# Create DynamoDB table
+resource "aws_dynamodb_table" "vault-data" {
+    name           = "vault-data"
+    read_capacity  = 2
+    write_capacity = 2
+    hash_key       = "Path"
+    range_key      = "Key"
+    attribute {
+        name = "Path"
+        type = "S"
+    }
+
+    attribute {
+        name = "Key"
+        type = "S"
+    }
+}
+
+# Create service account for vault. Should the policy
+resource "aws_iam_user" "vault" {
+  name = "vault_${var.region}"
+}
+
+data "aws_iam_policy_document" "vault" {
+    statement {
+        sid = "DynamoDB"
+        effect = "Allow"
+        actions = [
+            "dynamodb:DescribeLimits",
+            "dynamodb:DescribeTimeToLive",
+            "dynamodb:ListTagsOfResource",
+            "dynamodb:DescribeReservedCapacityOfferings",
+            "dynamodb:DescribeReservedCapacity",
+            "dynamodb:ListTables",
+            "dynamodb:BatchGetItem",
+            "dynamodb:BatchWriteItem",
+            "dynamodb:CreateTable",
+            "dynamodb:DeleteItem",
+            "dynamodb:GetItem",
+            "dynamodb:GetRecords",
+            "dynamodb:PutItem",
+            "dynamodb:Query",
+            "dynamodb:UpdateItem",
+            "dynamodb:Scan",
+            "dynamodb:DescribeTable"
+        ]
+        resources = ["${aws_dynamodb_table.vault-data.arn}"]
+    }
+    statement {
+        sid = "S3"
+        effect = "Allow"
+        actions = [
+                "s3:PutObject",
+                "s3:GetObject"
+        ]
+        resources = ["${aws_s3_bucket.vault-unseal.arn}/*"]
+    }
+    statement {
+        sid = "S3List"
+        effect = "Allow"
+        actions = [
+            "s3:ListBucket"
+        ]
+        resources = ["${aws_s3_bucket.vault-unseal.arn}"]
+    }
+    statement {
+        sid = "KMS"
+        effect = "Allow"
+        actions = [
+            "kms:Encrypt",
+            "kms:Decrypt",
+            "kms:DescribeKey"
+        ]
+        resources = ["${aws_kms_key.bank_vault.arn}"]
+    }
+}
+
+resource "aws_iam_user_policy" "vault" {
+    name = "vault_${var.region}"
+    user = "${aws_iam_user.vault.name}"
+
+    policy = "${data.aws_iam_policy_document.vault.json}"
+}
+
+resource "aws_iam_access_key" "vault" {
+    user = "${aws_iam_user.vault.name}"
+}
